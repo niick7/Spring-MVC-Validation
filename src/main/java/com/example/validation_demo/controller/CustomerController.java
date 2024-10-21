c30e512d0547f65ded2aca00acd66469d475e223
Nhan Vo <niick7@gmail.com>
2024-10-21 15:12:50 -0500

    Use InitBinder annotation to trim leading and trailing whitespaces for html inputs

--- src/main/java/com/example/validation_demo/controller/CustomerController.java
+++ src/main/java/com/example/validation_demo/controller/CustomerController.java
@@ -2,15 +2,28 @@ package com.example.validation_demo.controller;
 
 import com.example.validation_demo.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+    // Add an initBinder to convert trim input strings
+    // Remove leading and trailing whitespace
+    // Resolve issues for our validation
+    // Remember this method will apply for all requests for CustomerController
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/")
     public String showForm(Model model) {
         model.addAttribute("customer", new Customer());
